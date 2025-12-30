f04fa366443ec4708d2ffa2dce347a1653e22dde
Hooman <hoomanmoradnia@gmail.com>
2025-12-30 19:36:56 +0330

    Article detail route was changed.

--- personalWeblog/weblog/urls.py
+++ personalWeblog/weblog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'weblog'
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('<slug:slug>', views.article_detail, name='article_detail'),
 ]
