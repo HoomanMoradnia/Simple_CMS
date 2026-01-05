5e8474164ecd1be438e2699cdad678034bb90042
Hooman <hoomanmoradnia@gmail.com>
2026-01-05 23:30:46 +0330

    Static url was added.

--- personalWeblog/personalWeblog/settings.py
+++ personalWeblog/personalWeblog/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# static
+
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
